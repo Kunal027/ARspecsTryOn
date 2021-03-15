167adf6eb04e6568f0bce0001db53d634f4d3e80
Kunal Panigrahi <75083810+Kunal027@users.noreply.github.com>
2021-03-15 12:25:05 -0800

    Ui
    New popup UI panel for buy and cancel is created along with UI script

---
+++ Spec/Assets/UIcontroller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIcontroller : MonoBehaviour
+{
+    public GameObject g1info;
+    public GameObject g2info;
+    public GameObject g3info;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    public void G1InfoBoxEnable()
+    {
+        g1info.SetActive(true);
+    }
+
+    public void G2InfoBoxEnable()
+    {
+        g2info.SetActive(true);
+    }
+
+    public void G3InfoBoxEnable()
+    {
+        g3info.SetActive(true);
+    }
+
+    public void G1InfoBoxDisable()
+    {
+        g1info.SetActive(false);
+    }
+
+    public void G2InfoBoxDisable()
+    {
+        g2info.SetActive(false);
+    }
+
+    public void G3InfoBoxDisable()
+    {
+        g3info.SetActive(false);
+    }
+}
